0e4e3cc103e13d1fedd1b0fc24010d72eeb03229
gurukuldevops <67823840+gurukuldevops@users.noreply.github.com>
2023-08-12 21:11:16 +1000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Load variables from .env file
+export $(grep -v '^#' .env | xargs)
+
+docker-compose stop nginx
+
+certbot certonly --standalone -d $WEBSITE1_DOMAIN --agree-tos --email $EMAIL_ADDRESS
+certbot certonly --standalone -d $WEBSITE2_DOMAIN --agree-tos --email $EMAIL_ADDRESS
+certbot certonly --standalone -d $WEBSITE3_DOMAIN --agree-tos --email $EMAIL_ADDRESS
+
+docker-compose start nginx
