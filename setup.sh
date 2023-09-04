7b049e6e18814f10d5cd7238adb7fb01ae6bb29f
Pinaki Mukherjee <pinaki@mukherjee.com>
2023-09-04 19:54:21 +1000

    Update

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 # Load variables from .env file.
 export $(grep -v '^#' .env | xargs)
 
-service nginx stop | true
+docker-compose stop nginx
 
-certbot certonly --standalone -d $WEBSITE1_DOMAIN --agree-tos --email $EMAIL_ADDRESS
-certbot certonly --standalone -d $WEBSITE2_DOMAIN --agree-tos --email $EMAIL_ADDRESS
-certbot certonly --standalone -d $WEBSITE3_DOMAIN --agree-tos --email $EMAIL_ADDRESS
-
-service nginx start | true
+certbot certonly --standalone -d $WEBSITE1_DOMAIN --agree-tos --email $EMAIL_ADDRESS --keep-until-expiring
+certbot certonly --standalone -d $WEBSITE2_DOMAIN --agree-tos --email $EMAIL_ADDRESS --keep-until-expiring
+certbot certonly --standalone -d $WEBSITE3_DOMAIN --agree-tos --email $EMAIL_ADDRESS --keep-until-expiring
+ 
+docker-compose stop nginx
